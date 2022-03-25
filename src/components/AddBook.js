08ed2c18536d9e1b13420eb4dffd81b562c5a7b9
iLynette <lynetteacholah@icloud.com>
2022-03-25 14:01:48 +0300

    Remove uncommented code

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,25 +1,5 @@
 import React from 'react';
 
-// export default class AddBook extends React.Component {
-//   render() {
-//     return (
-//       <div className="add">
-//           <h3>ADD NEW BOOK</h3>
-//           <form className="add">
-//               <input type="text" placeholder="Book Title" className="bTitle"></input>
-//               <select key="bookCategory" className="field">
-//                   <option value="Category">Category</option>
-//                   <option value="Action">Action</option>
-//                   <option value="fiction">Science Fiction</option>
-//                   <option value="Economy">Economy</option>
-//               </select>
-//               <input type="submit" value="ADD BOOK" className="buttn"></input>
-//           </form>
-//       </div>
-//     );
-//   }
-// }
-
 const AddBook = () => (
   <div className="add">
     <h3>ADD NEW BOOK</h3>
