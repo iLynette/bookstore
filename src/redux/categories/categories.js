e7afb8b0d09585bab788fe5b5bb47c37f68e731b
iLynette <lynetteacholah@icloud.com>
2022-03-29 14:06:55 +0300

    Fix linters

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,17 @@
 const STATUS = 'bookstore/categories/status';
 
-const status = (status) => ({
-    type: STATUS,
-    status,
-  });
+export const status = (status) => ({
+  type: STATUS,
+  status,
+});
 
-  const categoriesReducer = (state = [], action) => {
-      switch (action.type) {
-          case STATUS:
-              return 'Under Construction';
-          default:
-              return state;
-      }
-  };
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case STATUS:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
 
-  export default categoriesReducer;
-
+export default categoriesReducer;
