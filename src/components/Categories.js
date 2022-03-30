89819e381fbe693710b9fce5689d0726f961cfaa
iLynette <lynetteacholah@icloud.com>
2022-03-30 21:31:37 +0300

    Add a buuton to the Categories component that dispatches the underconstruction action

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { status } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>Nothing to see here</div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(status())
+  }
+  return (
+    <div>
+      <button type="submit" className="submitBtn" handleClick={handleClick}>
+        Check Status
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
