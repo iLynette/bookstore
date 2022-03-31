e3b3a3b3710d669220e86023fade3eb6723a737f
iLynette <lynetteacholah@icloud.com>
2022-03-31 07:55:34 +0300

    Render status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { status } from '../redux/categories/categories';
 
 const Categories = () => {
   const dispatch = useDispatch();
+  const stats = useSelector((state) => state.categoriesReducer);
   return (
     <div>
-      <button type="submit" className="submitBtn" onClick={() => dispatch(status())}>
+      <button type="button" className="submitBtn" onClick={() => dispatch(status())}>
         Check Status
       </button>
+      {stats}
     </div>
   );
 };
